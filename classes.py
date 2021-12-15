95b0bca8b2e0ce280e0f28ac84adbaaae2905546
Brandon <bsmith0430@att.net>
2021-12-15 06:50:17 -0600

    added roll method

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+import random
 class Player(object):
     # Game variables
     name = ""
@@ -6,9 +7,10 @@ class Player(object):
     additional_dice = 0
     status = ""
     rolls_remaining = 0
+    locked_dice = []
     actions_available = []
 
-    def __init__(self, name, health_points, base_dice, 
+    def __init__(self, name, health_points, base_dice,
                 additional_dice, status, rolls_remaining, actions_available):
         self.name = name
         self.health_points = health_points
@@ -18,4 +20,13 @@ class Player(object):
         self.rolls_remaining = rolls_remaining
         self.actions = actions_available
 
-        
+    def roll(self, base_dice, additional_dice, rolls_remaining, status, locked_dice):
+        self.locked_dice = locked_dice
+        rolls = []
+        total_rolls = base_dice + additional_dice - len(locked_dice)
+        if self.rolls_remaining > 0:
+            for _ in range(total_rolls):
+                dice_roll = random.randint(1, 6)
+                rolls.append(dice_roll)
+                self.rolls_remaining -= 1
+                print(f"{self.name} rolled a {dice_roll}")
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ base_dice = 5
 additional_dice = 0
 status = "No status"
 rolls_remaining = 3
+locked_dice = []
 actions_available = []
 
 
@@ -24,3 +25,21 @@ while game_is_running:
         game_is_running = False
         print('Thanks for playing!')
         break
+
+    for player in player_names:
+        player = Player(player_names[player], player_health_points, base_dice, additional_dice,
+                        status, rolls_remaining, actions_available)
+
+        if player_health_points <= 0:
+            print(f"{player_names[player]} has lost the game")
+            game_is_running = False
+            break
+
+        user_input = input("What do you want to do? ")
+        if user_input == "q":
+            game_is_running = False
+            print('Thanks for playing!')
+            break
+
+        if user_input == "roll":
+            player.roll(player.base_dice, player.additional_dice, player.rolls_remaining, player.status, player.locked_dice)
